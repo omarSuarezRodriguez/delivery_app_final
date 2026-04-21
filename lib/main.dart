1364b68d8af67e82a8b7d71ec69e75e635e4d663
omarSuarezRod <omar.suarez.rod@gmail.com>
2026-04-21 01:20:10 -0500

    vid 16

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
+import 'package:delivery_app_final/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      // Nombre de la App, el ícono se muestra en el menú de aplicaciones del dispositivo
+      title: 'Delivery App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => LoginPage())
+      ],
+      navigatorKey: Get.key,
+
+
     );
   }
 }
