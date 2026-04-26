50f0985a15ffa0ad7a7edb6ba3866ad48e5e7aa9
omarSuarezRod <omar.suarez.rod@gmail.com>
2026-04-26 15:34:37 -0500

    vid 31

--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -1,22 +1,40 @@
+import 'package:delivery_app_final/src/models/response_api.dart';
+import 'package:delivery_app_final/src/providers/users_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  // Provider del user
+  UsersProvider usersProvider = UsersProvider();
 
   // Ir a la página de registro
   void goToRegisterpage() {
     Get.toNamed('/register');
   }
 
-
-  void login() {
+ 
+  void login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
     if (isValidForm(email, password)) {
-      Get.snackbar('Formulario válido', 'Estas listo para enviar la petición Http');
+
+      ResponseApi responseApi = await usersProvider.login(email, password);
+
+      print('Response Api: ${responseApi.toJson()}');
+
+      if (responseApi.success == true) {
+        Get.snackbar('Login exitoso', responseApi.message ?? ''); // Si viene nulo, mostramos el texto vació
+      }
+      else {
+        Get.snackbar('Login fallido', responseApi.message ?? '');
+      }
+
+      
     }
 
   }
--- lib/src/providers/users_provider.dart
+++ lib/src/providers/users_provider.dart
@@ -1,4 +1,5 @@
 import 'package:delivery_app_final/src/enviroment/environment.dart';
+import 'package:delivery_app_final/src/models/response_api.dart';
 import 'package:delivery_app_final/src/models/user.dart';
 import 'package:get/get.dart';
 
@@ -9,6 +10,7 @@ class UsersProvider extends GetConnect {
   String url = Environment.API_URL + 'api/users';
 
 
+  // Crear usuario
   Future<Response> create(User user) async {
     Response response = await post(
       '$url/create', 
@@ -19,6 +21,31 @@ class UsersProvider extends GetConnect {
     ); // Va a esperar hasta que el servidor nos retorne la respuesta
 
     return response;
+  }
+
+
+  //
+  Future<ResponseApi> login(String email, String password) async {
+    Response response = await post(
+      '$url/login', 
+      {
+        'email': email,
+        'password': password
+      }, 
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    ); // Va a esperar hasta que el servidor nos retorne la respuesta
+
+
+    if (response.body == null) {
+      Get.snackbar('Error', 'No se pudo ejecutar la petición');
+      return ResponseApi();
+    }
+
+    ResponseApi responseApi = ResponseApi.fromJson(response.body);
+
+    return responseApi;
   }
 
 
